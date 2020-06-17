ba77d9c496478266f6d1d3cdf0b292a4452e9395
dreamthen <dreamthen.00@gmail.com>
2020-06-17 21:51:17 +0800

    business:打包处理

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -17,6 +17,9 @@ class LoginDemo extends Component {
     navigationBarTitleText: '登录'
   };
 
+  componentDidMount() {
+  }
+
   render() {
     return (
       <View className='keryi-login'>
@@ -28,12 +31,8 @@ class LoginDemo extends Component {
           header={{
             'content-type': 'application/x-www-form-urlencoded'
           }}
-          callBack={(data, statusCode) => {
-            if (statusCode === 200) {
-              console.log(data);
-            } else {
-              console.log(data);
-            }
+          callBack={(data, header) => {
+            console.log(data);
           }}
           done={(res) => {
             console.log(res);

39112e40bfa5bab0bb181437538446b2330704a5
dreamthen <dreamthen.00@gmail.com>
2020-06-18 00:49:42 +0800

    build:打包外部登录组件

---
+++ src/components/OutLogin/index.js
@@ -0,0 +1,43 @@
+import Taro from "@tarojs/taro";
+import Ldanm from "../utils";
+
+export default function OutLogin({
+                                   url = '',
+                                   method = 'get',
+                                   header = {},
+                                   callBack = () => {
+                                   },
+                                   done = () => {
+                                   }
+                                 }) {
+  Taro.login({
+    success: ({code}) => {
+      Ldanm.request({
+        url,
+        method,
+        header,
+        data: {
+          code
+        },
+        success: (data, statusCode) => {
+          callBack(data, statusCode);
+        },
+        fail: (res = {}) => {
+          callBack(res);
+        },
+        complete: (res = {}) => {
+          done(res);
+        }
+      });
+    },
+    fail: (res = {}) => {
+      Taro.showModal({
+        title: '登录api错误',
+        content: '检测您是否处在小程序环境下调用此api',
+        showCancel: false
+      });
+    },
+    complete: (res = {}) => {
+    }
+  });
+}
--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ export { default as LdmUserInfo } from './components/UserInfo';
 export { default as LdmNavBar } from './components/NavBar';
 export { default as LdmTabBar } from './components/TabBar';
 export { default as LdmImagePicker } from './components/ImagePicker';
+export { default as LdmOutLogin } from './components/OutLogin';

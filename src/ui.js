346e6f1e459beb719ef6ec295409369ef39ad0a4
dreamthen <dreamthen.00@gmail.com>
2020-06-18 10:55:10 +0800

    build:限制上传组件的入口,npm 0.1.7版本更新

--- src/ui.js
+++ src/ui.js
@@ -4,7 +4,7 @@ Taro.initPxTransform({ designWidth: 750, deviceRatio: {} })
 
 export { default as LdmLogin } from './components/Login';
 export { default as LdmUserInfo } from './components/UserInfo';
-export { default as LdmNavBar } from './components/NavBar';
-export { default as LdmTabBar } from './components/TabBar';
-export { default as LdmImagePicker } from './components/ImagePicker';
+// export { default as LdmNavBar } from './components/NavBar';
+// export { default as LdmTabBar } from './components/TabBar';
+// export { default as LdmImagePicker } from './components/ImagePicker';
 export { default as LdmOutLogin } from './components/OutLogin';

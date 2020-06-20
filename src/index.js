5486160e7bda5531e1d537cba65f04b96ae3e12d
dreamthen <dreamthen.00@gmail.com>
2020-06-20 17:23:36 +0800

    build:添加navBar顶部自定义导航栏

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { default as LdmLogin } from './components/Login';
 export { default as LdmUserInfo } from './components/UserInfo';
-// export { default as LdmNavBar } from './components/NavBar';
+export { default as LdmNavBar } from './components/NavBar';
 // export { default as LdmTabBar } from './components/TabBar';
 // export { default as LdmImagePicker } from './components/ImagePicker';
 export { default as LdmOutLogin } from './components/OutLogin';

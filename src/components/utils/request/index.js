72d20776385f3eaa5677aee8698b754fc25619ed
dreamthen <dreamthen.00@gmail.com>
2020-10-27 14:36:08 +0800

    test:测试异常的提示语

--- src/components/utils/request/index.js
+++ src/components/utils/request/index.js
@@ -103,7 +103,6 @@ function uploadFile({
     header,
     formData,
     success({data, statusCode}) {
-      console.log(data);
       if (exception({
         errMsg: data['errmsg'],
         data: {data, statusCode},
